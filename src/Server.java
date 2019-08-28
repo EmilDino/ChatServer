9dd6699321f24e578b1295bda7cbb539f61b6fd2
EmilDino <EmilLD123@hotmail.com>
2019-08-28 12:04:10 +0200

    Har sat serveren op, så dne forhåbenlig kan håndtere flere clienter

--- src/Server.java
+++ src/Server.java
@@ -1,20 +1,86 @@
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.scene.Scene;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextArea;
 import javafx.stage.Stage;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Date;
+
 public class Server extends Application {
     String message;
+    private int clientNo = 0;
+    TextArea ta = new TextArea();
 
     @Override
     public void start(Stage primaryStage) {
-        TextArea ta = new TextArea();
 
         Scene scene = new Scene(new ScrollPane(ta), 450, 200);
         primaryStage.setTitle("Server");
         primaryStage.setScene(scene);
         primaryStage.show();
+
+        new Thread( () -> {
+            try {
+                ServerSocket serverSocket = new ServerSocket(8001);
+                ta.appendText("Server started at " + new Date() + '\n');
+
+                while (true) {
+                    Socket socket = serverSocket.accept();
+
+                    clientNo++;
+
+                    Platform.runLater( () -> {
+                        ta.appendText("Starting thread client " + clientNo + " at " + new Date() + '\n');
+
+                        InetAddress inetAddress = socket.getInetAddress();
+                        ta.appendText("Client " + clientNo + " host name is " + inetAddress.getHostName() + "\n");
+                        ta.appendText("Client " + clientNo + " IP Address is " + inetAddress.getHostAddress() + "\n");
+                    });
+
+                    new Thread(new HandleAClient(socket)).start();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }).start();
+    }
+
+    class HandleAClient implements Runnable {
+        private Socket socket;
+
+        public HandleAClient(Socket socket) {
+            this.socket = socket;
+        }
+        public void run() {
+            try {
+                DataInputStream inputFromClient = new DataInputStream(socket.getInputStream());
+                DataOutputStream outputToClient = new DataOutputStream(socket.getOutputStream());
+
+                while (true) {
+                    // Receive numbers from the client
+                    message = inputFromClient.readUTF();
+
+                    Platform.runLater(() -> {
+                        ta.appendText("Client: " + message + "\n");
+                        try {
+                            outputToClient.writeUTF(message);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    });
+                }
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     public static void main(String[] args) {
